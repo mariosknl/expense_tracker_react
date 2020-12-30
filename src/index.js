f25d0907d9040b248ddf3f88858d944b33e9e70f
mariosknl <mariosknl@outlook.com>
2020-12-30 20:29:53 +0200

    add speechly functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
 import { Provider } from "./context/context";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="bd248b12-d40a-4ff1-bab3-fad821e957a6" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 );
