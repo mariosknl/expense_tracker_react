88f2f256c52a8a4e42115be0fc8911efcfd36248
mariosknl <mariosknl@outlook.com>
2020-12-31 09:41:19 +0200

    add notification for every transaction created

--- src/context/context.js
+++ src/context/context.js
@@ -16,7 +16,11 @@ export const Provider = ({ children }) => {
   const addTransaction = (transaction) =>
     dispatch({ type: "ADD_TRANSACTION", payload: transaction });
 
-  console.log(transactions);
+  const balance = transactions.reduce((acc, currVal) => {
+    return currVal.type === "Expense"
+      ? acc - currVal.amount
+      : acc + currVal.amount;
+  }, 0);
 
   return (
     <ExpenseTrackerContext.Provider
@@ -24,6 +28,7 @@ export const Provider = ({ children }) => {
         deleteTransaction,
         addTransaction,
         transactions,
+        balance,
       }}
     >
       {children}
